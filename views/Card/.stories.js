423465e84f99896bd16feffdc1500a853914460b
Austin Wei <awei01@gmail.com>
2016-11-09 17:12:01 -0500

    adding Card

--- views/Card/.stories.js
+++ views/Card/.stories.js
@@ -1,161 +1,426 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
-import { makeLipsum, makeColorStory, makePassesPropsStory, extractOptionsValues } from '../../.storybook/utils'
-import { Story } from '../../.storybook/components'
-import { Card, Cards, Content } from './index'
+import { storiesOf, action, linkTo } from '@kadira/storybook'
+import { Story, Api, Example, makeLipsum } from '../../.storybook/components'
+import Card, { cardDefinition, contentDefinition, Cards, cardsDefinition } from './index'
+const { Image, Content } = Card
+const { Header, Meta, Description } = Content
+
+const _path = 'sui-stateless-components/views/Card'
 
 storiesOf('Card', module)
-  .add('<Card> default', () => {
+  .add('<Card />', () => {
     return (
-      <Story examples='<Card>[ content ]</Card>'>
-        <Card>
-          <Content>
-            This is the card content
-          </Content>
-        </Card>
-      </Story>
-    )
-  })
-  .add('<Card> color', makeColorStory(Card, {
-    props: {
-      children: (value) => {
-        return (<Content>This card is {value}</Content>)
-      }
-    }
-  }))
-  .add('<Cards>', () => {
-    return (
-      <Story examples='<Cards><Card/></Cards>'>
-        <Cards>
-          <Card>
-            <Content>
-              Card 1
-            </Content>
-          </Card>
-          <Card>
-            <Content>
-              Card 2
-            </Content>
-          </Card>
-          <Card>
-            <Content>
-              Card 3
-            </Content>
-          </Card>
-        </Cards>
-      </Story>
-    )
-  })  /*
-  .add('<Modal> active', () => {
-    return (
-      <Story examples='<Modal>[ content ]</Modal>' container={_container}>
-        <Modal active>
-          <Content>Some modal content here</Content>
-        </Modal>
-      </Story>
-    )
-  })
-  .add('<Modal> size="small"', () => {
-    return (
-      <Story examples='<Modal size="small"/>' showToggleNote container={_container}>
-        <Modal active size="small">
-          <Content>small modal</Content>
-        </Modal>
-      </Story>
-    )
-  })
-  .add('<Modal> size="large"', () => {
-    return (
-      <Story examples='<Modal size="large"/>' showToggleNote container={_container}>
-        <Modal active size="large">
-          <Content>large modal</Content>
-        </Modal>
-      </Story>
-    )
-  })
-  .add('<Modal> size="fullscreen"', () => {
-    return (
-      <Story examples='<Modal size="fullscreen"/>' showToggleNote container={_container}>
-        <Modal active size="fullscreen">
-          <Content>fullscreen modal</Content>
-        </Modal>
-      </Story>
-    )
-  })
-  .add('<Modal> scrolling', () => {
-    return (
-      <Story examples='<Modal scrolling/>' showToggleNote container={_container}
-          notes={['<Modal scrolling> should be wrapped in <Dimmer modals>',
-            '<Dimmer modals> should be immediate child of an element with className="scrolling dimmable dimmed"']}>
-        <div className='scrolling dimmable dimmed' style={{height: '1000px'}}>
-          <Dimmer page active modals>
-            <Modal active scrolling>
+      <Story title='<Card />'>
+        <Api definition={cardDefinition} otherProps={{
+          href: {
+            description: 'When present (even if {null}), the component will render as an <a /> tag'
+          }
+        }}>
+          import Card from '{_path}'
+        </Api>
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Image src='public/images/lindsay.png' />
               <Content>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
-                <p>{makeLipsum()}</p>
+                Some content
               </Content>
-            </Modal>
-          </Dimmer>
-
-        </div>
+            </Card>
+          </Example>
+          <Example title='[className] gets passed'>
+            <Card className='red'>
+              <Image src='public/images/lindsay.png' />
+              <Content>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='href'>
+          <Example>
+            <Card href='http://www.google.com' target='_blank'>
+              <Image src='public/images/lindsay.png' />
+              <Content>
+                card with href
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='fluid'>
+          <Example>
+            <Card fluid>
+              <Image src='public/images/lindsay.png' />
+              <Content>
+                fluid card
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='centered'>
+          <Example>
+            <Card centered>
+              <Image src='public/images/lindsay.png' />
+              <Content>
+                centered card
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='raised'>
+          <Example>
+            <Card raised>
+              <Image src='public/images/lindsay.png' />
+              <Content>
+                raised card
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='color'>
+          <Example.Iterator component={Card} propKey='color' definition={cardDefinition} props={{
+            children: (value) => {
+              return (<Content>{value} card</Content>)
+            }
+          }}/>
+        </Story.Segment>
       </Story>
     )
   })
-  .add('<Modal> passes .className and other props', () => {
+  .add('<Card.Image />', () => {
     return (
-      <Story examples='<Modal className="small" onClick={function}/>' showToggleNote container={_container}>
-        <Modal active className="small" onClick={ action('Modal was clicked')}>
-          <Content>Some modal content here</Content>
-        </Modal>
+      <Story title='<Card.Image />'>
+        <Api otherProps={{
+          src: {
+            description: 'When passed, the [children] prop will be ignored'
+          }
+        }} />
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Image>
+                <img src='public/images/lindsay.png' />
+              </Image>
+              <Content>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='src'>
+          <p>When present, this component will ignore any child content</p>
+          <Example>
+            <Card>
+              <Image src='public/images/lindsay.png'>
+                You will not see this content
+              </Image>
+              <Content>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
       </Story>
     )
   })
-  .add('<Header>', () => {
+  .add('<Card.Content />', () => {
     return (
-      <Story examples='<Modal><Header/></Modal>' container={_container}>
-        <Modal active>
-          <Header>modal header area</Header>
-        </Modal>
+      <Story title='<Card.Content />'>
+        <Api definition={contentDefinition} />
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Image>
+                <img src='public/images/lindsay.png' />
+              </Image>
+              <Content>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='extra'>
+          <Example>
+            <Card>
+              <Image src='public/images/lindsay.png'>
+                You will not see this content
+              </Image>
+              <Content>
+                Some content
+              </Content>
+              <Content extra>
+                Some extra content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
       </Story>
     )
   })
-  .add('<Content>', () => {
+  .add('<Card.Content.Header />', () => {
     return (
-      <Story examples='<Modal><Content/></Modal>' container={_container}>
-        <Modal active>
-          <Content>modal content area</Content>
-        </Modal>
+      <Story title='<Card.Content.Header />'>
+        <Api otherProps={{
+          href: {
+            description: 'When present (even if {null}), the component will render as an <a /> tag'
+          }
+        }} />
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Content>
+                <Header>some header</Header>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='href'>
+          <Example>
+            <Card>
+              <Content>
+                <Header href='http://www.google.com' target='_blank'>header with href</Header>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
       </Story>
     )
   })
-  .add('<Content> image', () => {
+  .add('<Card.Content.Meta />', () => {
     return (
-      <Story examples='<Modal><Content image><Image/><Description/></Content></Modal>' container={_container}>
-        <Modal active>
-          <Content image>
-            <Image src='public/images/lindsay.png'/>
-            <Description>
-              Some description here
-            </Description>
-          </Content>
-        </Modal>
+      <Story title='<Card.Content.Meta />'>
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Content>
+                <Header>some header</Header>
+                <Meta>some meta content</Meta>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
       </Story>
     )
   })
-  .add('<Actions> image', () => {
+  .add('<Card.Content.Description />', () => {
     return (
-      <Story examples='<Modal><Actions/></Modal>' container={_container}>
-        <Modal active>
-          <Actions>
-            modal actions area
-          </Actions>
-        </Modal>
+      <Story title='<Card.Content.Description />'>
+        <Story.Segment title='(default)'>
+          <Example>
+            <Card>
+              <Content>
+                <Header>some header</Header>
+                <Description>some description content</Description>
+                Some content
+              </Content>
+            </Card>
+          </Example>
+        </Story.Segment>
       </Story>
     )
   })
-*/
+  .add('<Cards />', () => {
+    return (
+      <Story title='<Cards />'>
+        <Api definition={cardsDefinition}>
+          {'import { Cards } from \'' + _path + '\''}
+        </Api>
+        <Story.Segment title='(default)'>
+          <Example>
+            <Cards>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+          <Example title='[className] gets passed'>
+            <Cards className='red'>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='raised'>
+          <Example>
+            <Cards raised>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='centered'>
+          <Example>
+            <Cards centered>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='link'>
+          <Example>
+            <Cards link>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='color'>
+          <Example>
+            <Cards color='red'>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/jenny.jpg' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='count'>
+          <Example>
+            <Cards count='four'>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+        <Story.Segment title='responsive'>
+          <Example title='stackable'>
+            <Cards count='four' responsive='stackable'>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+          <Example title='doubling'>
+            <Cards count='four' responsive='doubling'>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+              <Card>
+                <Image src='public/images/lindsay.png' />
+                <Content>
+                  Some content
+                </Content>
+              </Card>
+            </Cards>
+          </Example>
+        </Story.Segment>
+      </Story>
+    )
+  })
--- views/Card/index.js
+++ views/Card/index.js
@@ -1,70 +1,142 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { makeSuffixedClass, makeFactory, enums, options } from '../../utilities'
 import classnames from 'classnames'
-import { makeClassnameFactory, makeComponentWithClasses, OPTIONS } from '../../utils'
-import 'semantic-ui-css/components/card.css'
+import 'semantic-ui-css/components/card.css';
 
 /*
- |---------------------------
+ |-----------------------
+ | Shared options
+ |-----------------------
+ */
+
+/*
+ |-----------------------
  | Card
- |---------------------------
+ |-----------------------
  */
-const { color } = OPTIONS
-
-export const makeCardClasses = makeClassnameFactory({
-  prefix: 'ui',
-  suffix: 'card',
-  options: {
-    color
+export const cardDefinition = {
+  fluid: true,
+  centered: true,
+  raised: true,
+  color: enums.colors
+}
+const _cardFactory = makeFactory(cardDefinition)
+const Card = (props) => {
+  const [classes, rest] = _cardFactory.extractClassesAndProps(props)
+  const className = classnames('ui', classes, 'card')
+  let element = 'div'
+  if ('href' in rest) {
+    element = 'a'
   }
-})
-export const Card = (props) => {
-  const {
-    color,
-    className,
-    ...rest
-  } = props
-  const classes = makeCardClasses({ color }, className)
+  return React.createElement(element, { ...rest, className })
+}
+Card.propTypes = { ..._cardFactory.propTypes }
+Card.displayName = 'Card'
+export default Card
+
+/*
+ |-----------------------
+ | Card.Image
+ |-----------------------
+ */
+const Image = (props) => {
+  const { className, src, ...rest } = props
+  const classes = classnames(className, 'image')
+  const children = src ? (<img src={src} />) : props.children
   return (
-    <div {...rest} className={ classes } />
+    <div {...rest} className={classes}>
+      {children}
+    </div>
   )
 }
+Image.displayName = 'Card.Image'
+Card.Image = Image
 
 
 /*
- |---------------------------
+ |-----------------------
+ | Card.Content
+ |-----------------------
+ */
+export const contentDefinition = {
+  extra: true
+}
+const _contentFactory = makeFactory(contentDefinition)
+const Content = (props) => {
+  const [classes, rest] = _contentFactory.extractClassesAndProps(props)
+  const className = classnames(classes, 'content')
+  return (
+    <div {...rest} className={className} />
+  )
+}
+Content.propTypes = { ..._contentFactory.propTypes }
+Content.displayName = 'Card.Content'
+Card.Content = Content
+
+/*
+ |-----------------------
+ | Card.Content.Header
+ |-----------------------
+ */
+const Header = (props) => {
+  const className = classnames(props.className, 'header')
+  let element = 'div'
+  if ('href' in props) {
+    element = 'a'
+  }
+  return React.createElement(element, { ...props, className })
+}
+Header.displayName = 'Card.Content.Header'
+Content.Header = Header
+
+/*
+ |-----------------------
+ | Card.Content.Meta
+ |-----------------------
+ */
+const Meta = (props) => {
+  const className = classnames(props.className, 'meta')
+  return (
+    <div {...props} className={className} />
+  )
+}
+Meta.displayName = 'Card.Content.Meta'
+Content.Meta = Meta
+
+/*
+ |-----------------------
+ | Card.Content.Description
+ |-----------------------
+ */
+const Description = (props) => {
+  const className = classnames(props.className, 'description')
+  return (
+    <div {...props} className={className} />
+  )
+}
+Description.displayName = 'Card.Content.Description'
+Content.Description = Description
+
+/*
+ |-----------------------
  | Cards
- |---------------------------
+ |-----------------------
  */
-export const makeCardsClasses = makeClassnameFactory({
-  prefix: 'ui',
-  suffix: 'cards',
-})
+export const cardsDefinition = {
+  raised: true,
+  centered: true,
+  link: true,
+  color: enums.colors,
+  count: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+  responsive: ['stackable', 'doubling']
+}
+const _cardsFactory = makeFactory(cardsDefinition)
 export const Cards = (props) => {
-  const {
-    className,
-    ...rest
-  } = props
-  const classes = makeCardsClasses({}, className)
+  const [classes, rest] = _cardsFactory.extractClassesAndProps(props)
+  const className = classnames('ui', classes, 'cards')
   return (
-    <div {...rest} className={ classes } />
+    <div {...rest} className={className} />
   )
 }
-
-/*
- |---------------------------
- | Content
- |---------------------------
- */
-export const makeContentClasses = makeClassnameFactory({
-  suffix: 'content'
-})
-export const Content = (props) => {
-  const {
-    className,
-    ...rest
-  } = props
-  const classes = makeContentClasses({}, className)
-  return (
-    <div {...rest} className={ classes } />
-  )
-}
+Cards.propTypes = { ..._cardsFactory.propTypes }
+Cards.displayName = 'Cards'
